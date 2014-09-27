f670085000526a83db17c73b3f05b848598fae50
Sean Loper <Grimmdev@gmail.com>
2014-09-27 19:18:34 -0400

    Initial Release

---
+++ Source/Code/BackgroundScroll.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroll : MonoBehaviour
+{
+	public enum ScrollStyle { Left, Right };
+	public ScrollStyle scrollStyle = ScrollStyle.Left;
+	public float scrollSpeed = 1;
+	private float offset;
+
+	// Use this for initialization
+	void Awake ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		switch (scrollStyle) {
+			case ScrollStyle.Left:
+				offset = Time.time * scrollSpeed;
+			break;
+			case ScrollStyle.Right:
+				offset = Time.time * -scrollSpeed;
+			break;
+		}
+		renderer.material.mainTextureOffset = new Vector2 (offset, 0);
+	}
+}
---
+++ Source/Code/ObjectScroll.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectScroll : MonoBehaviour
+{
+	public enum ScrollStyle { Left, Right };
+	public ScrollStyle scrollStyle = ScrollStyle.Left;
+	public float scrollSpeed = 5;
+	private float move;
+	private Vector3 screenLimit;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		screenLimit = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height, 0));
+		screenLimit.x += this.transform.localScale.x / 2;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		switch (scrollStyle) {
+			case ScrollStyle.Left:
+				move = scrollSpeed * Time.deltaTime;  
+				transform.Translate (Vector3.left * move, Space.World);  
+				if (transform.position.x < -screenLimit.x) {  
+					transform.position = new Vector3 (screenLimit.x, transform.position.y, transform.position.z);  
+				}
+			break;
+			case ScrollStyle.Right:
+				move = -scrollSpeed * Time.deltaTime;  
+				transform.Translate (Vector3.left * move, Space.World);  
+				if (transform.position.x < -screenLimit.x) {  
+					transform.position = new Vector3 (screenLimit.x, transform.position.y, transform.position.z);  
+				}
+			break;
+		}
+	}
+}
